47a8aa3de900af7877a26c50fc3941fe76af5a1b
Catia Filiberto <cfiliber@e3r2p6.42roma.it>
2021-11-16 19:00:06 +0100

    16_11_2

--- pipex.h
+++ pipex.h
@@ -6,15 +6,12 @@
 /*   By: cfiliber <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 17:29:59 by cfiliber          #+#    #+#             */
-/*   Updated: 2021/11/16 17:40:01 by cfiliber         ###   ########.fr       */
+/*   Updated: 2021/11/16 18:57:16 by cfiliber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
-
-# define EXIT_FAILURE -1
-# define EXIT_SUCCESS 0
 
 # include <unistd.h>
 # include <stdio.h>
