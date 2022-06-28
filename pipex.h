726f9260592329e6d4d85bf1c1851846d17b7553
Catia Filiberto <cfiliber@e3r10p1.42roma.it>
2022-06-28 19:14:08 +0200

    ok

--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: cfiliber <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 17:29:59 by cfiliber          #+#    #+#             */
-/*   Updated: 2021/11/18 14:14:57 by cfiliber         ###   ########.fr       */
+/*   Updated: 2022/03/13 20:15:38 by cfiliber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,8 @@
 /* pipex.c */
 int		main(int argc, char **argv, char **envp);
 void	pipex(int infile, int outfile, char **argv, char **envp);
-void	child_one(int infile, char **argv, char **envp, int *end);
-void	child_two(int outfile, char **argv, char **envp, int *end);
+int	child_one(int infile, char **argv, char **envp, int *end);
+int	child_two(int outfile, char **argv, char **envp, int *end);
 
 /* execute.c */
 void	parsing_1(char *argv, char **envp);
